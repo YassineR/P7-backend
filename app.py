06a5f3b79502795c85030c8e89498ec1bad67738
YassineR <yassine.rachedi@yahoo.fr>
2023-09-24 17:04:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+import pickle
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Web App with Python Flask!'
+    return 'Add id client in route to have his data'
+
+
+@app.route('/<id_client>')
+def get_id_client(id_client):
+    return "The id is " + str(id_client)
+
+with open('lgbm_client_scoring.pkl', 'rb') as model_file:
+    model = model_file.read()
+
+@app.route('/get-model', methods=['GET'])
+def get_model():	
+    return model
+
